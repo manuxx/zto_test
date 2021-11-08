638a667357ec99fddc4a3fd19e0ce60f649e250a
manuxx <manus@pg.gda.pl>
2021-11-08 09:24:57 +0100

    first commit

---
+++ Spec/Spec.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Machine.Specifications;
+
+namespace Training.Spec
+{
+    public class test_concern 
+    {
+        static int sut;
+        const int initial_value = 5;
+
+        Establish context = () =>
+            sut = initial_value;
+
+        private Because of = () =>
+            sut = sut + 1;
+
+        It sut_should_be_propery_incremented = () =>
+            sut.ShouldEqual(initial_value+1);
+    }
+    
+}
+
